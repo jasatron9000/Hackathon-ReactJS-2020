089db69fa684881e19dae51d16f2a03046ff854f
Chung Hak Ngor <chunghakngor@Chungs-MacBook.local>
2020-08-09 09:55:33 +1200

    api upvotes/downvotes done

--- pages/api/downvotes.js
+++ pages/api/downvotes.js
@@ -12,11 +12,30 @@ handler.post(async (req, res) => {
         const userID = req.body.userID
         const comment = await req.db.collection('comments').findOne({_id: ObjectId(commentID)});
         if (comment.upVotes.includes(userID) || comment.upVotes.includes(userID)){
-            res.json({status: "you have already posted a comment before"})
+            res.json({status: "You cannot downvote a comment more than once"})
         } else {
             comment.downVotes.push(userID);
             await req.db.collection('comments').findOneAndUpdate({_id: ObjectId(commentID)}, {$set: comment});
             res.json({status: "success"})
+        }
+    } catch (err) {
+        console.log(err)
+        res.json({status: "error", err})
+    }
+})
+
+handler.delete(async (req, res) => {
+    try {
+        const commentID = req.body.commentID
+        const userID = req.body.userID
+        const comment = await req.db.collection('comments').findOne({_id: ObjectId(commentID)});
+        if (comment.upVotes.includes(userID) || comment.upVotes.includes(userID)){
+            const pos = comment.downVotes.indexOf(userID)
+            comment.downVotes.splice(pos)
+            await req.db.collection('comments').findOneAndUpdate({_id: ObjectId(commentID)}, {$set: comment});
+            res.json({status: "success"})
+        } else {
+            res.json({status: "error"})
         }
     } catch (err) {
         console.log(err)
