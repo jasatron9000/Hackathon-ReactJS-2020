cccfe2a3570c30e2ea107f7aac8754590f5aafc1
Shiyao <swan485@aucklanduni.ac.nz>
2020-08-09 14:28:22 +1200

    Gride can now fetch from api

--- components/ExamQPage_comps/MediaGrid.js
+++ components/ExamQPage_comps/MediaGrid.js
@@ -1,24 +1,47 @@
-import QuestionCard from "./QuestionCards";
+import QuestionCard from "../ExamQPage_comps/QuestionCards";
 import Grid from "@material-ui/core/Grid";
 import React, { useState, useEffect } from "react";
+import config from "../../lib/config";
 
 function MediaGrid(course, year) {
   const [ItemArray, setItemArray] = useState([]);
   //const [count, setCount] = useState(0);
 
+  // useEffect(() => {
+  //   fetch("https://kitsu.io/api/edge/anime?filter[text]=naruto")
+  //     // fetch("URL" + course + year)
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       console.log(response);
+  //       setItemArray(response.data);
+  //     })
+  //     .catch(() => console.log("it didn't work"));
+  // }, []);
+
+  const [classes, setClasses] = useState([]);
+  // fetch data
   useEffect(() => {
-    fetch("https://kitsu.io/api/edge/anime?filter[text]=naruto")
-      // fetch("URL" + course + year)
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        setItemArray(response.data);
-      })
-      .catch(() => console.log("it didn't work"));
+    const fetchClasses = async () => {
+      const res = await fetch(`${config.HOST}/api/classes/COMPSCI335`);
+      const classesJson = await res.json();
+      setClasses(classesJson);
+    };
+    fetchClasses();
+  }, []);
+
+  const [posts, setPosts] = useState([]);
+  // fetch data
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`${config.HOST}/api/posts`);
+      const postsJson = await res.json();
+      setPosts(postsJson);
+    };
+    fetchPosts();
   }, []);
 
   var Cards = [];
-  ItemArray.forEach((el, i) => {
+  posts.forEach((el, i) => {
     //console.log(ItemArray.length);
     if (el === undefined) {
       return;
@@ -26,10 +49,10 @@ function MediaGrid(course, year) {
       Cards.push(
         <Grid key={"card_" + i}>
           <QuestionCard
-            QuestionTitle={el["attributes"]["canonicalTitle"]}
-            Description={el["attributes"]["synopsis"]}
-            Name={el["attributes"]["slug"]}
-            Date={el["attributes"]["startDate"]}
+            QuestionTitle={el["title"]}
+            Description={el["body"]}
+            Name={el["userID"]}
+            Date={el["date"]}
           />
         </Grid>
       );
